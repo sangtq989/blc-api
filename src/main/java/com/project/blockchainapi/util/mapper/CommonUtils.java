d9d61506c91805821d43b8c24a02158c2d9bce53
sangphamd <sangphamd@nashtechglobal.com>
2023-06-20 01:38:04 +0700

    NPE when convert string to array

--- src/main/java/com/project/blockchainapi/util/mapper/CommonUtils.java
+++ src/main/java/com/project/blockchainapi/util/mapper/CommonUtils.java
@@ -1,10 +1,15 @@
 package com.project.blockchainapi.util.mapper;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class CommonUtils {
     public static List<String> convertStringToList(String value) {
+        if (Objects.isNull(value)) {
+            return new ArrayList<>();
+        }
         return Arrays.asList(value
                 .trim()
                 .substring(1, value.length() - 1)
